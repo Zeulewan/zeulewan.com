e4700100ee0fa219c7f2008d1b39ec126c2b1c13
Zeulewan <55005377+Zeulewan@users.noreply.github.com>
2022-05-10 16:57:18 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -25,6 +25,12 @@
 		<li data-tab-target="#something"    class="tab"><font color="FFFFFF">About me</font></li>
   	</ul>
 
+
+
+
+
+
+
   	<div class="tab-content">
     		<div id="home" data-tab-content class="active">
 		
@@ -33,11 +39,188 @@
 				<h4>This is where I'll post some of the things I do throughout the summer</h4><br>
 			
 				<?php 
-					for ($i=2022; $i>=2022; $i--) {
-						for ($j=12; $j>=1; $j--) {
-							for ($k=31; $k>= 1; $k--) {		
-								echo file_get_contents("blog/$i/$j/$k/$k.html");	
+					for ($i=2022; $i>=2022; $i--) 
+					{
+						for ($j=12; $j>=1; $j--) 
+						{
+							for ($k=31; $k>= 1; $k--) 
+							{		
+								$pagecontents = file_get_contents("blog/$i/$j/$k/$k.html");
+								if ($pagecontents!=null)
+								{
+									echo file_get_contents("format/a.html");
+
+									switch ($j) {
+										case 1:
+											if ($k==1){
+												echo "<b>January ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>January ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>January ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>January ${k}th $i</b> <br>";
+											}
+											break;
+										case 2:
+											if ($k==1){
+												echo "<b>February ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>February ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>February ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>February ${k}th $i</b> <br>";
+											}
+											break;
+										case 3:
+											if ($k==1){
+												echo "<b>March ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>March ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>March ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>March ${k}th $i</b> <br>";
+											}
+											break;
+										case 4:
+											if ($k==1){
+												echo "<b>April ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>April ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>April ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>April ${k}th $i</b> <br>";
+											}
+											break;
+										case 5:
+											if ($k==1){
+												echo "<b>May ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>May ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>May ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>May ${k}th $i</b> <br>";
+											}
+											break;
+										case 6:
+											if ($k==1){
+												echo "<b>June ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>June ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>June ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>June ${k}th $i</b> <br>";
+											}
+											break;
+										case 7:
+											if ($k==1){
+												echo "<b>July ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>July ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>July ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>July ${k}th $i</b> <br>";
+											}
+											break;	
+										case 8:
+											if ($k==1){
+												echo "<b>August ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>August ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>August ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>August ${k}th $i</b> <br>";
+											}
+											break;									
+										case 9:
+											if ($k==1){
+												echo "<b>September ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>September ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>September ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>September ${k}th $i</b> <br>";
+											}
+											break;
+										case 10:
+											if ($k==1){
+												echo "<b>October ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>October ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>October ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>October ${k}th $i</b> <br>";
+											}
+											break;
+										case 11:
+											if ($k==1){
+												echo "<b>November ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>November ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>November ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>November ${k}th $i</b> <br>";
+											}
+											break;
+										case 12:
+											if ($k==1){
+												echo "<b>December ${k}st $i</b> <br>";
+											} else if ($k==2){
+												echo "<b>December ${k}nd $i</b> <br>";
+											} else if ($k==3){
+												echo "<b>December ${k}rd $i</b> <br>";
+											} else{
+												echo "<b>December ${k}th $i</b> <br>";
+											}
+											break;		
+									
+									
+										}
+
+
+
+
+
+
+
+
+
+
+
+
+									$pagecontents = file_get_contents("blog/$i/$j/$k/$k.html");
+									jpg("1", "$pagecontents", "$i", "$j", "$k");
+								
+									echo file_get_contents("format/b.html");
+								}
 							}
+						}
+					}
+	
+					function jpg($l,$pc,$a,$b,$c)
+					{
+						
+						$x = str_replace("$l.jpg", "<br><br><img src='blog/$a/$b/$c/$l.jpg' alt='image not found'>", $pc);
+						$y = str_replace("$l.JPG", "<br><br><img src='blog/$a/$b/$c/$l.JPG' alt='image not found'>", $x);
+						$z = str_replace("$l.png", "<br><br><img src='blog/$a/$b/$c/$l.png' alt='image not found'>", $y);	
+						$q = str_replace("$l.jpeg", "<br><br><img src='blog/$a/$b/$c/$l.jpeg' alt='image not found'>", $z);	
+
+						if($l<5){	
+							jpg($l+1,$q,$a,$b,$c);
+						} 
+						else 
+						{	
+							echo $q;
 						}
 					}
 				?>
